776a3031de0a564ae85d9bea26b4f2be6c900a21
Tejinderpal Singh <tejinderp105@gmail.com>
2026-04-07 22:17:23 +0530

    cleanup

--- ColabX-main/colabXFrontend/src/components/headers/DashboardHeader.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export function DashboardHeader() {
-    return (
-        <header className="w-full h-14 flex items-center px-10 border-b border-white/5 sticky top-0 z-50 bg-barckground/70 backdrop-blur-xl shadow-sm">
-            <h1>DashboardHeader</h1>
-        </header>
-    );
-}
--- ColabX-main/colabXFrontend/src/utils/auth-client.ts
+++
@@ -1,8 +0,0 @@
-import { createAuthClient } from "better-auth/react"
-import { AUTH_BASE } from "@/lib/api"
-
-export const authClient = createAuthClient({
-  baseURL: AUTH_BASE,
-})
-
-export const { signIn, signUp, useSession } = authClient
