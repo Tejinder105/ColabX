b3abbdccba24920744bbab899fd0028ef5af95ca
Tejinderpal Singh <tejinderp105@gmail.com>
2026-04-18 19:08:13 +0530

    Remove Table 1 and Database ER Diagram documentation; streamline database schema representation and normalization details.

--- colabXBackend/generate-migration.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Generate the correct migration for dropping partnerScore and performanceMetric tables
-
-cd d:/ColabX/colabXBackend
-
-# Generate migration based on schema changes
-npx drizzle-kit generate:pg --name drop_performance_tables
-
